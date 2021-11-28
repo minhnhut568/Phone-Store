160a554744f905a5d5e04ae76ea82f3f2b4987f1
Minh Nhut <16520904@gm.uit.edu.vn>
2021-11-28 10:02:36 +0700

    upload source code

---
+++ resources/lang/vn/footer.php
@@ -0,0 +1,14 @@
+<?php
+
+  return [
+    'AboutUs'          =>      'Về Chúng Tôi',
+    'address'          =>      'Khu phố 6 - phường Linh Trung - quận Thủ Đức - thành phố Hồ Chí Minh',
+    'FollowUs'         =>      'Theo Dõi Chúng Tôi',
+    'visa'             =>      'Thanh toán bằng thẻ Visa.',
+    'mastercard'       =>      'Thanh toán bằng thẻ MasterCard.',
+    'jcb'              =>      'Thanh toán bằng thẻ JCB.',
+    'cod'              =>      'Thanh toán khi nhận hàng.',
+    'nganluong'        =>      'Giải pháp thanh toán qua Ngân Lượng.',
+    'payment'          =>      'Hỗ Trợ Thanh Toán',
+    'support'          =>      'Hỗ Trợ Khách Hàng',
+  ];
